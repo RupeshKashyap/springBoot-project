f7efa2d3b223da5c0473707f81f5c79bcc6ad98f
RupeshKashyap <rupeshkashyap1996@gmail.com>
2022-06-09 17:46:43 +0530

    Demo Application

---
+++ springBoot/demo/src/test/java/com/rupesh/appRunner/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rupesh.appRunner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
